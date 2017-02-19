e26c9bffc8da649e14f731d2c0e1d96368684dd0
tobias <thinkdifferent055@gmail.com>
2017-02-19 21:05:04 +0100

    Improve Code, add new protocol text file

--- src/main/scala/de/tobias/playpad/server/server/project/sync/ProjectSyncHandler.scala
+++ src/main/scala/de/tobias/playpad/server/server/project/sync/ProjectSyncHandler.scala
@@ -17,6 +17,8 @@ import scala.collection.{Map, mutable}
   */
 @WebSocket class ProjectSyncHandler(sessionDao: Dao[account.Session, Int], connection: Connection) {
 
+	val SESSION_KEY_HEADER = "key"
+
 	// TODO mutable.HashSet --> Set
 	private var sessions: Map[Account, mutable.HashSet[Session]] = new mutable.HashMap[Account, mutable.HashSet[Session]]()
 
@@ -27,33 +29,33 @@ import scala.collection.{Map, mutable}
 	)
 
 	@OnWebSocketConnect def onConnect(serverSession: Session): Unit = {
-		val key = serverSession.getUpgradeRequest.getHeader("key")
+		val key = serverSession.getUpgradeRequest.getHeader(SESSION_KEY_HEADER)
 		if (key == null) {
 			serverSession.close(500, "Invalid Key")
 		}
 
-		val sessions = sessionDao.queryForEq("key", key)
-		if (sessions.size() == 1) {
-			val session = sessions.get(0)
-			if (!this.sessions.contains(session.getAccount)) {
-				this.sessions += (session.getAccount -> new mutable.HashSet[Session]())
-			}
-			this.sessions(session.getAccount) += serverSession
-		} else {
-			serverSession.close(500, "Invalid Key")
+		val session = account.Session.getSession(key, sessionDao)
+		session match {
+			case Some(s) =>
+				if (!this.sessions.contains(s.getAccount)) {
+					this.sessions += (s.getAccount -> new mutable.HashSet[Session]())
+				}
+				this.sessions(s.getAccount) += serverSession
+			case None => serverSession.close(500, "Invalid Key")
 		}
 	}
 
 	@OnWebSocketClose def onClose(serverSession: Session, status: Int, reason: String): Unit = {
-		val key = serverSession.getUpgradeRequest.getHeader("key")
+		val key = serverSession.getUpgradeRequest.getHeader(SESSION_KEY_HEADER)
 		if (key == null) {
 			serverSession.close(500, "Invalid Key")
 		}
 
-		val sessions = sessionDao.queryForEq("key", key)
-		if (sessions.size() == 1) {
-			val session = sessions.get(0)
-			this.sessions(session.getAccount) -= serverSession
+		val session = account.Session.getSession(key, sessionDao)
+		session match {
+			case Some(s) =>
+				this.sessions(s.getAccount) -= serverSession
+			case None => serverSession.close(500, "Invalid Key")
 		}
 	}
 
@@ -62,7 +64,7 @@ import scala.collection.{Map, mutable}
 		// Store in Database
 		try {
 			// Push to clients
-			val key = serverSession.getUpgradeRequest.getHeader("key")
+			val key = serverSession.getUpgradeRequest.getHeader(SESSION_KEY_HEADER)
 			if (key != null) {
 
 				val parser = new JsonParser()
@@ -80,12 +82,13 @@ import scala.collection.{Map, mutable}
 				serverSession.close(500, "Invalid Key")
 			}
 
-			val sessions = sessionDao.queryForEq("key", key)
-			if (sessions.size() == 1) {
-				val session = sessions.get(0)
-				this.sessions(session.getAccount)
-					.filter(s => s != serverSession)
-					.foreach(s => s.getRemote.sendStringByFuture(text))
+			val session = account.Session.getSession(key, sessionDao)
+			session match {
+				case Some(s) =>
+					this.sessions(s.getAccount)
+						.filter(s => s != serverSession)
+						.foreach(s => s.getRemote.sendStringByFuture(text))
+				case None => serverSession.close(500, "Invalid Key")
 			}
 		} catch {
 			case e: Exception => e.printStackTrace()
